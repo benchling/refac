a026673010b4412a7f756e42b037450bb451348c
Vivek Dasari <vivek.dasari@benchling.com>
2022-09-22 20:17:08 +0000

    Use setup.py instead of pyproject.toml

--- setup.py
+++ setup.py
@@ -1,3 +1,62 @@
-from setuptools import setup
+# https://hynek.me/articles/sharing-your-labor-of-love-pypi-quick-and-dirty
+import re
+from pathlib import Path
 
-setup()
+from setuptools import setup, find_packages
+
+ROOT_DIR = Path(__file__).parent.resolve()
+
+###################################################################
+
+NAME = "move-symbol"
+PACKAGES = find_packages(where="src")
+META_PATH = ROOT_DIR.joinpath("src/move_symbol/__init__.py")
+KEYWORDS = ["refactor", "symbol", "move"]
+CLASSIFIERS = [
+    "Intended Audience :: Developers",
+    "Natural Language :: English",
+    "Programming Language :: Python :: 3",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+]
+INSTALL_REQUIRES = ["click", "libcst"]
+
+###################################################################
+
+
+META_FILE = META_PATH.read_text()
+
+
+def find_meta(meta):
+    """
+    Extract __*meta*__ from META_FILE.
+    """
+    meta_match = re.search(
+        r"^__{meta}__ = ['\"]([^'\"]*)['\"]".format(meta=meta), META_FILE, re.M
+    )
+    if meta_match:
+        return meta_match.group(1)
+    raise RuntimeError("Unable to find __{meta}__ string.".format(meta=meta))
+
+
+if __name__ == "__main__":
+    setup(
+        name=NAME,
+        description=find_meta("description"),
+        license=find_meta("license"),
+        url=find_meta("uri"),
+        version=find_meta("version"),
+        author=find_meta("author"),
+        author_email=find_meta("email"),
+        maintainer=find_meta("author"),
+        maintainer_email=find_meta("email"),
+        keywords=KEYWORDS,
+        long_description=(ROOT_DIR / "README.md").read_text(),
+        long_description_content_type="text/markdown",
+        packages=PACKAGES,
+        package_dir={"": "src"},
+        zip_safe=False,
+        classifiers=CLASSIFIERS,
+        install_requires=INSTALL_REQUIRES,
+        options={"bdist_wheel": {"universal": "1"}},
+        entry_points={"console_scripts": ["move-symbol = move_symbol.__main__"]},
+    )
---
+++ src/move_symbol/__init__.py
@@ -0,0 +1,6 @@
+__description__ = "Tool to move symbols around in Python."
+__license__ = "TBD"
+__uri__ = "https://github.com/benchling/move-symbol"
+__version__ = "0.0.1"
+__author__ = "Benchling Eng"
+__email__ = "eng@benchling.com"
