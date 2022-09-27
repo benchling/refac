6eff9c6fe8c7b53db0d8bf24190e452a47167e87
Vivek Dasari <vivek.dasari@benchling.com>
2022-09-27 23:13:23 +0000

    add_symbols works with Importitem

--- src/move_symbol/visitors/add_symbols.py
+++ src/move_symbol/visitors/add_symbols.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-from dataclasses import dataclass, field
 from typing import Union
 
 import libcst as cst
@@ -7,7 +5,7 @@ from libcst.codemod import (
     CodemodContext,
     VisitorBasedCodemodCommand,
 )
-from libcst.codemod.visitors import AddImportsVisitor
+from libcst.codemod.visitors import AddImportsVisitor, ImportItem
 
 
 class AddSymbolsVisitor(VisitorBasedCodemodCommand):
@@ -19,7 +17,7 @@ class AddSymbolsVisitor(VisitorBasedCodemodCommand):
         symbols_to_add: set[
             Union[cst.FunctionDef, cst.ClassDef, cst.SimpleStatementLine]
         ],
-        imports_to_add: set[str],
+        imports_to_add: set[ImportItem],
     ) -> None:
         super().__init__(context)
         self.symbols_to_add = symbols_to_add
@@ -28,10 +26,10 @@ class AddSymbolsVisitor(VisitorBasedCodemodCommand):
     def leave_Module(
         self, original_node: cst.Module, updated_node: cst.Module
     ) -> cst.Module:
-        for associated_imports in self.imports_to_add:
-            # TODO: asname
-            module, obj = associated_imports.rsplit(".", 1)
-            AddImportsVisitor.add_needed_import(self.context, module, obj)
+        for item in self.imports_to_add:
+            AddImportsVisitor.add_needed_import(
+                self.context, item.module, item.obj_name, item.alias
+            )
         return updated_node.with_changes(
             body=[*updated_node.body, *self.symbols_to_add]
         )
